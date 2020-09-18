7f76a07c1d361aeece2b1f75af3303828819bafe
Alisa L <lungsuaya2@gmail.com>
2020-09-17 22:34:56 -0400

    Outline HTML

---
+++ script.js
@@ -0,0 +1,21 @@
+var key = "514ab56c2f48ab29cd4bd385635e22dc";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+userInputCity= [];
+
+
+
+
+
+
+
+// console.log(response);
+//       $(".city").html("<h1>" + response.name + "</h1>");
+
+//       $(".wind").text(response.wind.speed);
+
+//       $(".humidity").text(response.main.humidity);
+
+//       $(".temp").text(response.main.temp);
+     
+//       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+//       $(".tempF").text("temperature in (F) " + tempF.toFixed(2));
